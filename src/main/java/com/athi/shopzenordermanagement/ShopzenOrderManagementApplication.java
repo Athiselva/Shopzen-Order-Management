e609e6f2bd3e0b4e7b33c45a75547b609a5af457
Athiselvam N <athitek@rediffmail.com>
2023-11-01 12:09:51 +0530

    Initial commit

---
+++ src/main/java/com/athi/shopzenordermanagement/ShopzenOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.athi.shopzenordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopzenOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopzenOrderManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/athi/shopzenordermanagement/ShopzenOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athi.shopzenordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopzenOrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
